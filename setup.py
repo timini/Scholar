e9d0af877725addeca457328c2ec2f586b122542
tim <tim@localhost.localdomain>
2013-03-15 16:29:19 +0000

    install

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='Scholar',
     version='0.1.0',
     author='Christian Kreibich',
-    scripts=['scholar.py'],
+    py_modules=['scholar.py'],
     url='http://www.icir.org/christian/scholar.html',
     description='Python script for accessing Google Scholar, since it doesn\'t have an api.',
     long_description=open('README.md').read(),
